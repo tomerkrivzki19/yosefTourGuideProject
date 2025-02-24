0e51a9a9f527b89c0b9330f9257f3e0debc7ff22
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2025-02-24 18:43:50 +0200

    main page setup

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from "react";
+
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+  { name: "Log in", href: "#" },
+];
+function Nav() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [bgColor, setBgColor] = useState("transparent");
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      //   setBgColor("rgba(255, 255, 255, 0.9)"); // Change to your desired color or rgba for transparency
+      setBgColor("#FFFFFF");
+    } else {
+      setBgColor("transparent");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    // <header className="fixed inset-x-0 top-0 z-50"
+    <header
+      className="fixed inset-x-0 top-0 z-50 transition-all duration-300"
+      style={{ backgroundColor: bgColor }}
+    >
+      <div className="mx-auto max-w-7xl">
+        <div className="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
+          <nav
+            aria-label="Global"
+            className="flex items-center justify-between lg:justify-start"
+          >
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt="Your Company"
+                src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700 lg:hidden"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
+            <div className="hidden lg:ml-12 lg:flex lg:gap-x-14">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-sm/6 font-semibold text-gray-900"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </nav>
+        </div>
+      </div>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt=""
+                src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-8 w-auto"
+              />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  );
+}
+
+export default Nav;
