363d438ab0de83c6c3032a9abacc58e4705953b5
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2025-02-26 14:29:06 +0200

    logo addon on sm sizes nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -107,13 +107,14 @@ function Nav() {
           dir="rtl"
         >
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              {/* <img
+              <img
                 alt=""
-                src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              /> */}
+                // src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                src={logoBrand}
+                className="h-16 w-auto"
+              />
             </a>
             <button
               type="button"
